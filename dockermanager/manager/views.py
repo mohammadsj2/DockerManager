8948ed7c56f4c906196d9802066cecf808ce8d1c
mohammadsj2 <mohammad.sj2@gmail.com>
2021-12-29 02:57:15 +0330

    Add proper views classes' template

--- dockermanager/manager/views.py
+++ dockermanager/manager/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from knox.auth import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
+from rest_framework.response import Response
 
-# Create your views here.
+
+class app_list(generics.GenericAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        pass
+
+
+class create_app(generics.GenericAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        pass
+
+
+class get_app(generics.GenericAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, id):
+        pass
+
+
+class delete_app(generics.GenericAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, id):
+        pass
+
+
+class edit_app(generics.GenericAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, id):
+        pass
