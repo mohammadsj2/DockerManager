c2d05984c3eb30aa9e54cdc2dcda915f40d855bf
mohammadsj2 <mohammad.sj2@gmail.com>
2021-12-31 03:37:25 +0330

    Modify urls in order to be compatible with view classes

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Docker Manager
+This is a docker manager system. It allows you to control your 
+remote docker server. 
+
--- dockermanager/manager/urls.py
+++ dockermanager/manager/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('apps/', views.app_list, name='app_list'),
-    path('apps/create/', views.create_app, name='create_app'),
-    path('apps/<int:id>/', views.get_app, name='get_app'),
-    path('apps/<int:id>/delete/', views.delete_app, name='delete_app'),
-    path('apps/<int:id>/edit/', views.edit_app, name='edit_app'),
+    path('apps/', views.AppList.as_view(), name='app_list'),
+    path('apps/create/', views.CreateApp.as_view(), name='create_app'),
+    path('apps/<int:app_id>/', views.GetApp.as_view(), name='get_app'),
+    path('apps/<int:app_id>/delete/', views.DeleteApp.as_view(), name='delete_app'),
+    path('apps/<int:app_id>/edit/', views.EditApp.as_view(), name='edit_app'),
+    path('apps/<int:app_id>/containers/', views.ContainerList.as_view(), name='container_list'),
+    path('apps/<int:app_id>/containers/create/', views.CreateContainer.as_view(), name='create_container'),
 ]
